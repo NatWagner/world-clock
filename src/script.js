dcb8d2f5151ca93b0f399a5c3644cadc3b03e40f
NatWagner <124263343+NatWagner@users.noreply.github.com>
2023-04-30 14:23:18 +0200

    Added Interval for the selected location

--- src/script.js
+++ src/script.js
@@ -41,25 +41,27 @@ function updateTime() {
 
 //select
 function updateCity(event) {
-  let cityTimeZone = event.target.value;
-  if (cityTimeZone === "current") {
-    cityTimeZone = moment.tz.guess();
-  }
-  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
-  let cityTime = moment().tz(cityTimeZone);
-  let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = `
+  setInterval(() => {
+    let cityTimeZone = event.target.value;
+    if (cityTimeZone === "current") {
+      cityTimeZone = moment.tz.guess();
+    }
+    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+    let cityTime = moment().tz(cityTimeZone);
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = `
           <div class="city">
             <div>
               <h2>${cityName}</h2>
               <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
             </div>
             <div class="time">${cityTime.format(
-              "h:mm"
+              "h:mm:ss"
             )} <small>${cityTime.format("A")}</small></div>
           </div>
           <a class= backLink href="index.html"><i class="fa-solid fa-clock-rotate-left"></i></i> back</a>
          `;
+  }, 1000);
 }
 
 updateTime();
